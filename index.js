78214bc49815585f7e677df06a81ce8b7955f511
Eric Redon <eric.redon@coox.org>
2017-10-22 21:04:09 +0200

    Escape backticks when generating template literal

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(content) {
   this.cacheable && this.cacheable();
   this.value = content;
-  return "import css from 'styled-jsx/css';\n\nexport default css`" + content + "`;";
+  return "import css from 'styled-jsx/css';\n\nexport default css`" + content.replace(/`/g, '\\`') + "`;";
 };
