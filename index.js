70d9abcd288a546a9db2d1ba045922ef357163d2
Eric Redon <eric.redon@coox.org>
2017-11-05 12:43:40 +0100

    Escape backticks and backslashes: “`” ⇒ “\`”, “\” ⇒ “\\”

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 module.exports = function(content) {
   this.cacheable && this.cacheable();
   this.value = content;
-  return "import css from 'styled-jsx/css';\n\nexport default css`" + content.replace(/`/g, '\\`') + "`;";
+
+  // Escape backticks and backslashes: “`” ⇒ “\`”, “\” ⇒ “\\”
+  var escapedContent = content.replace(/[`\\]/g, function(match) {
+    return "\\" + match;
+  });
+
+  return "import css from 'styled-jsx/css';\n\nexport default css`" + escapedContent + "`;";
 };
